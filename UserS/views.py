d4e773b4b396f78449d8852d2319d2b1f1d5297d
kabuiya <kabuiyamonicah@gmail.com>
2022-08-24 12:00:25 +0300

    initial commit

---
+++ UserS/views.py
@@ -0,0 +1,52 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from knox.auth import AuthToken
+from .serializers import RegisterSerializer
+
+
+# from django.http import JsonResponse
+def serialize_user(user):
+    return {
+        "username": user.username,
+        "email": user.email,
+        "first_name": user.first_name,
+        "last_name": user.last_name
+    }
+
+
+@api_view(['POST'])
+def login_api(request):
+    serializer = AuthTokenSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    user = serializer.validated_data['user']
+    _, token = AuthToken.objects.create(user)
+
+    return Response({
+        'user_data': serialize_user(user),
+        'token': token
+    })
+
+
+@api_view(['GET'])
+def get_user_data(request):
+    user = request.user
+    if user.is_authenticated:
+        return Response({
+            'user_data': serialize_user(user)
+          })
+    return Response({})
+
+
+@api_view(['POST'])
+def register_api(request):
+    serializer = RegisterSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+
+    user = serializer.save()
+    _, token = AuthToken.objects.create(user)
+
+    return Response({
+        "user_info": serialize_user(user),
+        'token': token
+    })
